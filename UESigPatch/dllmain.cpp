21c9a3134d308e96a53e0a8926aa80f4a264f80a
Irastris <irastris15@gmail.com>
2024-09-04 16:24:28 -0400

    Add support for Quidditch

--- UESigPatch/dllmain.cpp
+++ UESigPatch/dllmain.cpp
@@ -4,7 +4,7 @@
 #include "utils.h"
 #include <string>
 
-constexpr const char* ASI_VERSION = "1.5.1";
+constexpr const char* ASI_VERSION = "1.5.2";
 
 bool bPauseOnStart = false;
 bool bShowConsole = false;
@@ -18,6 +18,7 @@ enum eSupportedGames {
 	eGameSifu,
 	eGameSouthPark,
 	eGameSuicideSquad,
+	eGameQuidditch,
 	eUnsupportedGame
 };
 
@@ -30,6 +31,7 @@ eSupportedGames GetEnumeratorFromProcessName(std::string const& sProcessName) {
 	if (sProcessName == "Sifu-Win64-Shipping.exe") return eGameSifu;
 	if (sProcessName == "SnowDay-Win64-Shipping.exe") return eGameSouthPark;
 	if (sProcessName == "SuicideSquad_KTJL.exe") return eGameSuicideSquad;
+	if (sProcessName == "QuidditchChampions-Win64-Shipping.exe") return eGameQuidditch;
 	return eUnsupportedGame;
 }
 
@@ -58,7 +60,6 @@ void CreateConsole()
 bool Initialize()
 {
 	const char* pSigCheck;
-	const char* pSigWarn;
 	const char* pChunkSigCheck;
 	const char* pChunkSigCheckFunc;
 	const char* pTOCCheck;
@@ -154,6 +155,18 @@ bool Initialize()
 
 			DisableTOCCompare(pTOCCompare);
 			
+			break;
+
+		case eGameQuidditch:
+			pSigCheck = "80 B9 ? ? ? ? 00 49 8B F0 48 8B FA 48 8B D9 75";
+			pChunkSigCheck = "0F B6 51 ? 48 8B F1 48 8B 0D ? ? ? ? E8 ? ? ? ? C6 46 ? ? 0F AE F8";
+			pChunkSigCheckFunc = "48 89 5C 24 ? 48 89 6C 24 ? 48 89 74 24 ? 57 41 56 41 57 48 83 EC 20 48 8D 59 08 49 63 F9 48 8B F1 49 8B E8 48 8B CB 44 0F B6 F2";
+			pTOCCheck = "4D 8B CE 45 33 C0 E8 ? ? ? ? 44 39 AD ? ? ? ? 0F 84";
+
+			DisableSignatureCheck(pSigCheck, 0x14);
+			DisableChunkSigCheck(pChunkSigCheck, pChunkSigCheckFunc);
+			DisableTOCSigCheck(pTOCCheck, 0x12);
+
 			break;
 
 		case eUnsupportedGame:
